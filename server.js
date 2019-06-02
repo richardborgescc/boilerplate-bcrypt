716fbf2bfe76a0513c2ffe2b1e002baece17568a
Richard Borges <richardborgescc@users.noreply.github.com>
2019-06-02 12:03:53 -0300

    Information Security And Quality Assurance. BCrypt: Comparing hash.

--- server.js
+++ server.js
@@ -13,9 +13,12 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash); //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); //true
+  });
+});
 //END_ASYNC
 
 //START_SYNC
