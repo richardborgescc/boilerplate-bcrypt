776582270ba8d017f05b382626e7e20cbd6e1ff6
Richard Borges <richardborgescc@users.noreply.github.com>
2019-06-03 13:15:24 -0300

    Information Security And Quality Assurance. BCrypt: Sync hash comparing.

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //END_ASYNC
 
 //START_SYNC
-
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result); 
 //END_SYNC
 
 
